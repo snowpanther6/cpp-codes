ce758e1385339b1eea4fb87c3af51c96d5f3329f
Shailesh Yogendra <53175687+snowpanther6@users.noreply.github.com>
2020-10-22 13:57:32 +0530

    template

---
+++ cpp template.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ff              first
+#define ss              second
+#define int             long long int
+#define pb              push_back
+#define mp12            make_pair
+#define pii             pair<int,int>
+#define vi              vector<int>
+#define mii             map<int,int>
+#define pq123           priority_queue<int>
+#define pqr1            priority_queue<int,vi,greater<int> >
+#define setbits(x)      __builtin_popcountll(x)
+#define zerobits(x)      __builtin_ctzll(x)
+#define mod             1000000007
+#define inf             1e18
+#define ps(x,y)         fixed<<setprecision(y)<<x
+#define mk(arr,n,type)  type *arr=new type[n];
+#define w(x)            int x; cin>>x; while(x--)
+
+void FIO(){
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+}
+
+int modInv(int a,int b,int m){int res=1;a=a%m;while(b>0){if(b&1){res=(res*a)%m;}a=(a*a)%m;b=b>>1;}return res;}
+
+
+
+void solve(){
+    
+    
+
+}
+    
+
+int32_t main()
+{
+    FIO();
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    solve();
+
+
+    return 0;
+}
